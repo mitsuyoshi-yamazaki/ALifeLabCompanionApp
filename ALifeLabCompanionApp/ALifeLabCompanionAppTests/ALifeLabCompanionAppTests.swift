9e869ea6b7031f2ed4cd77a315d120a55b490a8a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-11-25 22:27:32 +0900

    Added xcode project

---
+++ ALifeLabCompanionApp/ALifeLabCompanionAppTests/ALifeLabCompanionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ALifeLabCompanionAppTests.swift
+//  ALifeLabCompanionAppTests
+//
+//  Created by Yamazaki Mitsuyoshi on 2024/11/25.
+//
+
+import Testing
+@testable import ALifeLabCompanionApp
+
+struct ALifeLabCompanionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ALifeLabCompanionApp/ALifeLabCompanionAppUITests/ALifeLabCompanionAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ALifeLabCompanionAppUITestsLaunchTests.swift
+//  ALifeLabCompanionAppUITests
+//
+//  Created by Yamazaki Mitsuyoshi on 2024/11/25.
+//
+
+import XCTest
+
+final class ALifeLabCompanionAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
