a413704ce486a50fc531d8f459366465318f1078
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-11-25 23:45:31 +0900

    Implemented download functionality

--- ALifeLabCompanionApp/ALifeLabCompanionApp/ViewController.swift
+++ ALifeLabCompanionApp/ALifeLabCompanionApp/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+@preconcurrency import WebKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView! {
+        didSet {
+            webView.navigationDelegate = self
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        webView.load(.init(url: .init(string: "https://mitsuyoshi-yamazaki.github.io/ALifeLab/pages/drawer.html?system.run=0&system.auto_download=0")!))
     }
-
-
 }
 
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.shouldPerformDownload {
+            decisionHandler(.download)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+
+    func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
+        download.delegate = self
+    }
+}
+
+extension ViewController: WKDownloadDelegate {
+    func download(_ download: WKDownload, decideDestinationUsing response: URLResponse, suggestedFilename: String, completionHandler: @escaping (URL?) -> Void) {
+        let fileManager = FileManager.default
+        if let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let destinationURL = documentsURL.appendingPathComponent(suggestedFilename)
+            print("save downloading file to \(suggestedFilename)")
+            completionHandler(destinationURL)
+        } else {
+            print("unable to find save location")
+            completionHandler(nil)
+        }
+    }
+
+    func downloadDidFinish(_ download: WKDownload) {
+        print("download finished: \(download)")
+    }
+
+    func download(_ download: WKDownload, didFailWithError error: any Error, resumeData: Data?) {
+        print("download failed with error: \(error)")
+    }
+}
